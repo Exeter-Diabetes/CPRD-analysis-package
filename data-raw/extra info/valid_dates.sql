6d164571f7030fabf976cd10934f890bfa30933d
Katie Young <k.young3@exeter.ac.uk>
2022-01-17 09:59:26 +0000

    Update valid_dates.sql

--- data-raw/extra info/valid_dates.sql
+++ data-raw/extra info/valid_dates.sql
@@ -2,9 +2,9 @@
 
 set role 'role_full_admin';
 
-# drop table if exists cprd_data.r_valid_date_lookup;
+drop table if exists cprd_data.r_valid_date_lookup;
 
-create table cprd_data.r_valid_date_lookup as select c.patid, min_dob,
+create table cprd_data.r_valid_date_lookup as select c.patid, min_dob,gp_end_date,ons_death,hes_death_all,hes_death_nph_filtered,
 least(if(gp_end_date is null,str_to_date('1/1/2050','%d/%m/%Y'),gp_end_date), 
 if(ons_death is null,str_to_date('1/1/2050','%d/%m/%Y'),ons_death)) as gp_ons_end_date,
 least(if(gp_end_date is null,str_to_date('1/1/2050','%d/%m/%Y'),gp_end_date), 
@@ -12,10 +12,7 @@ if(ons_death is null,str_to_date('1/1/2050','%d/%m/%Y'),ons_death),
 if(hes_death_all is null,str_to_date('1/1/2050','%d/%m/%Y'),hes_death_all)) as gp_ons_hes_all_end_date,
 least(if(gp_end_date is null,str_to_date('1/1/2050','%d/%m/%Y'),gp_end_date), 
 if(ons_death is null,str_to_date('1/1/2050','%d/%m/%Y'),ons_death),
-if(hes_death_filter1 is null,str_to_date('1/1/2050','%d/%m/%Y'),hes_death_filter1)) as gp_ons_hes_filter1_end_date,
-least(if(gp_end_date is null,str_to_date('1/1/2050','%d/%m/%Y'),gp_end_date), 
-if(ons_death is null,str_to_date('1/1/2050','%d/%m/%Y'),ons_death),
-if(hes_death_filter2 is null,str_to_date('1/1/2050','%d/%m/%Y'),hes_death_filter2)) as gp_ons_hes_filter2_end_date from
+if(hes_death_nph_filtered is null,str_to_date('1/1/2050','%d/%m/%Y'),hes_death_nph_filtered)) as gp_ons_hes_nph_filtered_end_date from
 (select patid, min_dob, least(if(cprd_ddate is null,str_to_date('1/1/2050','%d/%m/%Y'),cprd_ddate), 
 if(regenddate is null,str_to_date('1/1/2050','%d/%m/%Y'),regenddate), 
 if(lcd is null,str_to_date('1/1/2050','%d/%m/%Y'),lcd)) as gp_end_date from 
@@ -24,21 +21,25 @@ as T1)
 as c left join (select patid, if(dod is null, dor, dod) as ons_death from cprd_data.ons_death) d on c.patid=d.patid left join 
 (select patid, min(discharged) as hes_death_all from cprd_data.hes_hospital where dismeth=4 and disdest=79 group by patid)
 e on c.patid=e.patid left join
-(select patid, min(hes_death) as hes_death_filter1 from
+(select patid, min(hes_death) as hes_death_nph_filtered from
 (select f.patid, n_patid_hes, hes_death from cprd_data.hes_patient f inner join
 (select patid, discharged as hes_death from cprd_data.hes_hospital where dismeth=4 and disdest=79)
 g on f.patid=g.patid where n_patid_hes<20)
 as T2 group by patid)
-h on c.patid=h.patid left join
-(select patid, min(hes_death) as hes_death_filter2 from
-(select i.patid, n_patid_hes, hes_death from cprd_data.hes_patient i inner join
-(select patid, discharged as hes_death from cprd_data.hes_hospital where dismeth=4 and disdest=79)
-j on i.patid=j.patid where n_patid_hes=1)
-as T3 group by patid)
-k on c.patid=k.patid;
+h on c.patid=h.patid;
 
-select * from cprd_data.r_valid_date_lookup where year(gp_ons_end_date)=2050 or year(gp_ons_hes_all_end_date)=2050 or year(gp_ons_hes_filter1_end_date)=2050 or year(gp_ons_hes_filter2_end_date)=2050;
+select * from cprd_data.r_valid_date_lookup where year(gp_ons_end_date)=2050 or year(gp_ons_hes_all_end_date)=2050 or year(gp_ons_hes_nph_filtered_end_date)=2050;
 # None
 
-select max(gp_ons_end_date), max(gp_ons_hes_all_end_date), max(gp_ons_hes_filter1_end_date), max(gp_ons_hes_filter2_end_date) from cprd_data.r_valid_date_lookup;
+select max(gp_ons_end_date), max(gp_ons_hes_all_end_date), max(gp_ons_hes_all_end_date) from cprd_data.r_valid_date_lookup;
 # All are 15/10/2020
+
+
+create unique index x_patid_r_valid_date_lookup on cprd_data.r_valid_date_lookup (patid);
+create index x_gp_end_date_r_valid_date_lookup on cprd_data.r_valid_date_lookup (gp_end_date);
+create index x_ons_death_r_valid_date_lookup on cprd_data.r_valid_date_lookup (ons_death);
+create index x_hes_death_all_r_valid_date_lookup on cprd_data.r_valid_date_lookup (hes_death_all);
+create index x_hes_death_nph_filtered_r_valid_date_lookup on cprd_data.r_valid_date_lookup (hes_death_nph_filtered);
+create index x_gp_ons_end_date_r_valid_date_lookup on cprd_data.r_valid_date_lookup (gp_ons_end_date);
+create index x_gp_ons_hes_all_end_date_r_valid_date_lookup on cprd_data.r_valid_date_lookup (gp_ons_hes_all_end_date);
+create index x_gp_ons_hes_nph_filtered_end_date_r_valid_date_lookup on cprd_data.r_valid_date_lookup (gp_ons_hes_nph_filtered_end_date);
