87518a6817ee0335537e35051007e34f0d0cbf23
Katie Young <k.young3@exeter.ac.uk>
2022-09-16 18:36:32 +0100

    Added eGFR function

--- R/CPRDBiomarkerAnalysis.R
+++ R/CPRDBiomarkerAnalysis.R
@@ -53,6 +53,32 @@ clean_biomarker_values = function(dataset, biomrkr_col, biomrkr) {
 }
 
 
+#' Calculates eGFR from creatinine, sex and age at creatinine reading using 2021 CKD-EPI Creatinine (https://www.mdcalc.com/calc/3939/ckd-epi-equations-glomerular-filtration-rate-gfr#evidence)
+#' 
+#' @description see above
+#' @param dataset - dataset containing creatinine, sex, and age at creatinine reading
+#' @param creatinine_col - column with creatinine reading in umol/L
+#' @param sex_col - column with sex: "male" or "female"
+#' @param age_at_creatinine_col - column with age at creatinine reading in years
+#' @export
+
+ckd_epi_2021_egfr = function(dataset, creatinine_col, sex_col, age_at_creatinine_col) {
+  
+  new_dataset <- dataset %>%
+    mutate(creatinine_mgdl=!!creatinine_col*0.0113) %>%
+    mutate(ckd_epi_2021_egfr=ifelse(creatinine_mgdl<=0.7 & !!sex_col=="female",(142 * ((creatinine_mgdl/0.7)^-0.241) * (0.9938^!!age_at_creatinine_col) * 1.012),
+                       ifelse(creatinine_mgdl>0.7 & !!sex_col=="female",(142 * ((creatinine_mgdl/0.7)^-1.2) * (0.9938^!!age_at_creatinine_col) * 1.012),
+                              ifelse(creatinine_mgdl<=0.9 & !!sex_col=="male",(142 * ((creatinine_mgdl/0.9)^-0.302) * (0.9938^!!age_at_creatinine_col)),
+                                     ifelse(creatinine_mgdl>0.9 & !!sex_col=="male",(142 * ((creatinine_mgdl/0.9)^-1.2) * (0.9938^!!age_at_creatinine_col)),NA))))) %>%
+    select(-creatinine_mgdl)
+  
+  message("New column 'ckd_epi_2021_egfr' added")
+  
+  return(new_dataset)
+
+}
+
+
 #' Impute missing predictors for QRISK2 and QDiabetes-HF
 #' 
 #' @description impute BMI, SBP and chol_HDL ratio where missing for QRISK2-2017 and QDiabetes-HF-2015
