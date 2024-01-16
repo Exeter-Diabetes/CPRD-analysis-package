e3cbf9976974eaf917699faddcefaea2a51d6aa4
Katie Young <75259370+drkgyoung@users.noreply.github.com>
2024-01-16 11:45:06 +0000

    Update setup-databases.sql

--- data-raw/setup-databases.sql
+++ data-raw/setup-databases.sql
@@ -1,14 +1,18 @@
-# Create the original data sets
+# CPRD Aurum extracts (datasets) and databases
+## Diabetes dataset from October 2023: data in cprd_data, analysis in cprd_analysis_dev (cprd_data_dev used for testing loading; cprd_analysis not used)
+## Full dataset from May 2021: data in full_cprd_data, analysis in full_cprd_analysis_dev (full_cprd_data_dev used for testing loading; full_cprd_analysis not used)
+## Diabetes dataset from February 2024: data in cprd_dm24_data, analysis in cprd_dm24_analysis (cprd_dm24_data_dev used for testing loading)
+
+# Create the databases
 # CREATE SCHEMA IF NOT EXISTS `cprd_data_dev` DEFAULT CHARACTER SET latin1 COLLATE latin1_general_ci;
 # CREATE SCHEMA IF NOT EXISTS `cprd_data` DEFAULT CHARACTER SET latin1 COLLATE latin1_general_ci;
 # CREATE SCHEMA IF NOT EXISTS `cprd_analysis_dev` DEFAULT CHARACTER SET latin1 COLLATE latin1_general_ci;
 # CREATE SCHEMA IF NOT EXISTS `cprd_analysis` DEFAULT CHARACTER SET latin1 COLLATE latin1_general_ci;
 
-# Create the new data sets
-CREATE SCHEMA IF NOT EXISTS `full_cprd_data_dev` DEFAULT CHARACTER SET latin1 COLLATE latin1_general_ci;
-CREATE SCHEMA IF NOT EXISTS `full_cprd_data` DEFAULT CHARACTER SET latin1 COLLATE latin1_general_ci;
-CREATE SCHEMA IF NOT EXISTS `full_cprd_analysis_dev` DEFAULT CHARACTER SET latin1 COLLATE latin1_general_ci;
-CREATE SCHEMA IF NOT EXISTS `full_cprd_analysis` DEFAULT CHARACTER SET latin1 COLLATE latin1_general_ci;
+# CREATE SCHEMA IF NOT EXISTS `full_cprd_data_dev` DEFAULT CHARACTER SET latin1 COLLATE latin1_general_ci;
+# CREATE SCHEMA IF NOT EXISTS `full_cprd_data` DEFAULT CHARACTER SET latin1 COLLATE latin1_general_ci;
+# CREATE SCHEMA IF NOT EXISTS `full_cprd_analysis_dev` DEFAULT CHARACTER SET latin1 COLLATE latin1_general_ci;
+# CREATE SCHEMA IF NOT EXISTS `full_cprd_analysis` DEFAULT CHARACTER SET latin1 COLLATE latin1_general_ci;
 
 # Create the roles:
 # 1) Loader role
